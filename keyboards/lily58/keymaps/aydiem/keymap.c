2b8bb13302dd7920ac12b4ce7c1b9c3e06ea396e
Aydiem <3894484+aydiem@users.noreply.github.com>
2020-09-19 00:33:00 -0700

    switched keycodes in keymap for raise/lower & enter/space.

--- keyboards/lily58/keymaps/aydiem/keymap.c
+++ keyboards/lily58/keymaps/aydiem/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                        KC_LALT, KC_LGUI, MO(_LOWER), KC_ENT, KC_SPC, MO(_RAISE), KC_BSPC, KC_RGUI \
+                        KC_LALT, KC_LGUI, KC_ENT, MO(_LOWER), MO(_RAISE), KC_SPC, KC_BSPC, KC_RGUI \
 ),
 /* GAMING
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -209,3 +209,4 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
+tes
