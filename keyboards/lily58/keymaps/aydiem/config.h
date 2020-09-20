35c7a83b6ac543abdb97da84fbea1fbfd94288f0
Aydiem <3894484+aydiem@users.noreply.github.com>
2020-09-20 00:30:14 -0700

    Removed define statement for rgbled animations in config.h.

--- keyboards/lily58/keymaps/aydiem/config.h
+++ keyboards/lily58/keymaps/aydiem/config.h
@@ -36,9 +36,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Underglow
 #undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 10
-#define RGBLIGHT_LIMIT_VAL 120
+#define RGBLIGHT_LIMIT_VAL 120 //change this to higher number if not bright enough at max
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
