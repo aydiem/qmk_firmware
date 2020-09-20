4b0aefaddde999168a962f7e97ede3f295b52cfb
Aydiem <3894484+aydiem@users.noreply.github.com>
2020-09-19 23:56:47 -0700

    Disabled keylogger in rules.mk. Also cleaned up/removed comments/unused consts in keymap.c

--- keyboards/lily58/keymaps/aydiem/keymap.c
+++ keyboards/lily58/keymaps/aydiem/keymap.c
@@ -150,24 +150,15 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 // When you add source files to SRC in rules.mk, you can use functions.
 const char *read_layer_state(void);
 const char *read_logo(void);
-void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
 
-// const char *read_mode_icon(bool swap);
 // const char *read_host_led_state(void);
-// void set_timelog(void);
-// const char *read_timelog(void);
 
 void oled_task_user(void) {
   if (is_keyboard_master()) {
     // If you want to change the display of OLED, you need to change here
     oled_write_ln(read_layer_state(), false);
-    // oled_write_ln(read_keylog(), false);
-    // oled_write_ln(read_keylogs(), false);
-    //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
     //oled_write_ln(read_host_led_state(), false);
-    //oled_write_ln(read_timelog(), false);
+
   } else {
     oled_write(read_logo(), false);
   }
