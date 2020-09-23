440daf9a97b3145a968e1deb4f8f8391a6f2ca1f
Aydiem <3894484+aydiem@users.noreply.github.com>
2020-09-23 16:15:15 -0700

    up to date keymap with tapping force hold turned off and per key tapping term added

--- keyboards/lily58/keymaps/aydiem/config.h
+++ keyboards/lily58/keymaps/aydiem/config.h
@@ -31,9 +31,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL_PD2
 
-#define TAPPING_FORCE_HOLD
+//#define TAPPING_FORCE_HOLD
 #define PERMISSIVE_HOLD
 #define TAPPING_TERM 150
+#define TAPPING_TERM_PER_KEY
 
 // Underglow
 #undef RGBLED_NUM
--- keyboards/lily58/keymaps/aydiem/keymap.c
+++ keyboards/lily58/keymaps/aydiem/keymap.c
@@ -22,44 +22,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  =   |
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  =   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |CAPS  |  A   |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |CAPS/CTL| A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|  MUTE |  Play/Pause|------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
- * `-----------------------------------------/ Enter /     \ Space\-----------------------------------------'
- *                   | LGUI |LCTRL | LAlt | /LOWER  /       \RAISE \  |BackSP|Delete| RGUI |
+ * `-----------------------------------------/ Enter /     \ BSPC  \-----------------------------------------'
+ *                   | LGUI | LAlt |LCTRL | /LOWER  /       \RAISE \  |Space |Delete| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
   [_QWERTY] = LAYOUT( \
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-  KC_CAPS,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL, \
+  LCTL_T(KC_CAPS),  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,   KC_MPLY, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-              KC_LGUI, KC_LCTRL, KC_LALT, LT(_LOWER, KC_ENT), LT(_RAISE, KC_SPC), KC_BSPC, KC_DEL, KC_RGUI \
+              KC_LGUI, KC_LALT, KC_LCTRL, LT(_LOWER, KC_ENT), LT(_RAISE, KC_BSPC), KC_SPC, KC_DEL, KC_RGUI \
 ),
 /* GAMING
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  =   |
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  =   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |CAPS  |  A   |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|  MUTE |  Play/Pause|------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
- * `-----------------------------------------/ Enter /     \ Space\-----------------------------------------'
- *                   | LCTRL| LAlt |Space | /LOWER  /       \RAISE \  |BackSP|Delete| RGUI |
+ * `-----------------------------------------/ Enter /     \ BSPC \-----------------------------------------'
+ *                   | LCTRL| LAlt |Space | /LOWER  /       \RAISE \  |Space |Delete| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
   [_GAMING] = LAYOUT( \
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
+  KC_CAPS, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
   _______,_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
                             KC_LCTRL, KC_LALT, KC_SPC, _______, _______, _______, _______, _______ \
 ),
@@ -69,11 +69,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                     |------+------+------+------+------+------|
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                     |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                     |------+------+------+------+------+------|
- * |   `  |   !  |   @  |   #  |   $  |   %  |-------.     ,-------|   ^  |   &  |   *  |   (  |   )  |   -  |
- * |------+------+------+------+------+------|MUTE   |   Play/Pause|------+------+------+------+------+------|
+ * |   `  |   !  |   @  |   #  |   $  |   %  |-------.     ,-------|   ^  |   &  |   *  |   (  |   )  |   ~  |
+ * |------+------+------+------+------+------|Play/Pause Play/Pause|------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|     |-------|      |   _  |   +  |   {  |   }  |   |  |
- * `-----------------------------------------/ Enter /      \ Space\-----------------------------------------'
- *                   | LGUI |LCTRL | LAlt | /LOWER  /        \RAISE \  |BackSP|Delete| RGUI |
+ * `-----------------------------------------/ Enter /      \ BSPC \-----------------------------------------'
+ *                   | LGUI | LAlt |LCTRL | /LOWER  /        \RAISE \  |Space |Delete| RGUI |
  *                   |      |      |      |/       /          \      \ |      |      |      |
  *                   `----------------------------'            '------''--------------------'
  */
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
   KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD, \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPLY, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
                              _______, _______, _______, _______, _______,  _______, _______, _______\
 ),
 /* RAISE
@@ -94,8 +94,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      | Home | PgUp |      |-------.    .-------|      | Left | Down |  Up  |Right |      |
  * |------+------+------+------+------+------|Play/Pause Ply/Pause|------+------+------+------+------+------|
  * |LShift|Insert|      | End  | PgDn |      |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |   \  |
- * `-----------------------------------------/ Enter /     \ Space\-----------------------------------------'
- *                   | LGUI |LCTRL | LAlt | /LOWER  /       \RAISE \  |BackSP|Delete| RGUI |
+ * `-----------------------------------------/ Enter /     \ BSPC \-----------------------------------------'
+ *                   | LGUI | LAlt |LCTRL | /LOWER  /       \RAISE \  |Space |Delete| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -115,8 +115,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |GAMING|-------.    ,-------|RGB ON| MODE+| HUE+ | SAT+ | VAL+ |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |LSHIFT|      |      |      |      |      |-------|    |-------|      | MODE-| HUE- | SAT- | VAL- |RSHIFT|
- * `-----------------------------------------/ Enter /     \ Space\-----------------------------------------'
- *                   | LGUI |LCTRL | LAlt | /LOWER  /       \RAISE \  |BackSP|Delete| RGUI |
+ * `-----------------------------------------/ Enter /     \ BSPC \-----------------------------------------'
+ *                   | LGUI | LAlt |LCTRL | /LOWER  /       \RAISE \  |Space |Delete| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -136,6 +136,16 @@ void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
   } else {
     layer_off(layer3);
   }
+}
+
+// set per key tapping terms here
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LCTL_T(KC_CAPS):
+            return 200;
+        default:
+            return TAPPING_TERM;
+    }
 }
 
 //SSD1306 OLED update loop, make sure to enable OLED_DRIVER_ENABLE=yes in rules.mk
@@ -194,20 +204,36 @@ static void render_logo(void) {
 // Add layer names as cases here to have them displayed on OLED
 const char *read_layer_state(void) {
     switch (get_highest_layer(layer_state)) {
-        case _QWERTY: return "Layer: QWERTY";
-        case _GAMING: return "Layer: Gaming";
-        case _LOWER: return "Layer: Lower";
-        case _RAISE: return "Layer: Raise";
-        case _ADJUST: return "Layer: Adjust";
-        default: return "Unknown";
+        case _QWERTY: 
+          return "Layer: QWERTY";
+          break;
+        case _GAMING: 
+          return "Layer: Gaming";
+          break;
+        case _LOWER: 
+          return "Layer: Lower";
+          break;
+        case _RAISE: 
+          return "Layer: Raise";
+          break;
+        case _ADJUST: 
+          return "Layer: Adjust";
+          break;
+        default: 
+          return "Unknown";
+          break;
     }
 }
 
+// If you want to change the display of OLED, you need to change here
 void oled_task_user(void) {
   if (is_keyboard_master()) {
-    // If you want to change the display of OLED, you need to change here
     oled_write_ln(read_layer_state(), false);
-    //oled_write_ln(read_host_led_state(), false);
+
+    //write the state of capslock and scroll lock
+    uint8_t led_usb_state = host_keyboard_leds();
+    oled_write_ln_P(IS_LED_ON(led_usb_state, USB_LED_CAPS_LOCK) ? PSTR("CAPSLOCK ON") : PSTR(""), false);
+    oled_write_ln_P(IS_LED_ON(led_usb_state, USB_LED_SCROLL_LOCK) ? PSTR("SCROLL LOCK ON") : PSTR(""), false);
 
   } else {
     //oled_write(read_logo(), false);
