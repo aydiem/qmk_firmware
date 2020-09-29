3c4454c016a65ba5c938cb64e95e8f4bd05c1d19
Aydiem <3894484+aydiem@users.noreply.github.com>
2020-09-28 19:35:11 -0700

    layout changes for nav, fixed ctrlshiftesc, and altf4.

--- keyboards/lily58/keymaps/aydiem/keymap.c
+++ keyboards/lily58/keymaps/aydiem/keymap.c
@@ -109,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 /* LOWER
  * ,-----------------------------------------.                     ,-----------------------------------------.
- * |      |      |      |      |      |      |                     |      |      |      |      |      |      |
+ * |      |      |      |      |AltF4 |      |                     |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                     |------+------+------+------+------+------|
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                     |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                     |------+------+------+------+------+------|
@@ -123,9 +123,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
   [_LOWER] = LAYOUT( \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, A(KC_F4),XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-  KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD, \
+  KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPLY, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
                              _______, _______, _______, _______, _______,  _______, _______, _______\
 ),
@@ -135,24 +135,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      | Home | PgUp |      |-------.    .-------|      | Left | Down |  Up  |Right |      |
+ * |      |      |      |LShift| LCTRL| PgUp |-------.    .-------| Home | Left | Down |  Up  |Right | End  |
  * |------+------+------+------+------+------|Play/Pause Ply/Pause|------+------+------+------+------+------|
- * |LShift|Insert|      | End  | PgDn |      |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |   \  |
+ * |LShift| Undo |  Cut | Copy |Paste | PgDn |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |   \  |
  * `-----------------------------------------/ Enter /     \ BSPC \-----------------------------------------'
  *                   | LGUI |Delete|LCTRL | /LOWER  /       \RAISE \  |Space |Delete| RGUI |
  *                   |      | LAlt |      |/       /         \      \ |      |RCTRL |      |
  *                   `----------------------------'           '------''--------------------'
  */
   [_RAISE] = LAYOUT( \
-  KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                          KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0, XXXXXXX, \
-  XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGUP, XXXXXXX,                     XXXXXXX, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, \
-  _______,  KC_INS, XXXXXXX, KC_END, KC_PGDN, XXXXXXX,  KC_MPLY, _______,  KC_PLUS, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, \
+  KC_F1,   KC_F2,     KC_F3,     KC_F4,     KC_F5,      KC_F6,                       KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, \
+  KC_GRV,  KC_1,      KC_2,      KC_3,      KC_4,       KC_5,                        KC_6,    KC_7,    KC_8,   KC_9,    KC_0,    XXXXXXX, \
+  XXXXXXX, XXXXXXX,   XXXXXXX,   KC_LSFT,   KC_LCTRL,   KC_PGUP,                     KC_HOME, KC_LEFT, KC_DOWN,KC_UP,   KC_RGHT, KC_END, \
+  _______, LCTL(KC_Z),LCTL(KC_X),LCTL(KC_C),LCTL(KC_V), KC_PGDN,  KC_MPLY, _______,  KC_PLUS, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, \
                              _______, _______, _______,  _______, _______,  _______, _______, _______ \
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |CSDEL |      |      |      |AltF4 |      |                    |      |      |      |PrintS|ScrLck| Pause|
+ * |CSESC |      |      |      |      |      |                    |      |      |      |PrintS|ScrLck| Pause|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |CADEL |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -165,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
   [_ADJUST] = LAYOUT( \
-  C(S(KC_DEL)), XXXXXXX, XXXXXXX, XXXXXXX, A(KC_F4),   XXXXXXX,                 XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_SLCK, KC_PAUSE, \
+  C(S(KC_ESC)), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,                 XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_SLCK, KC_PAUSE, \
   C(A(KC_DEL)), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
   XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, TG(_NUMP), TG(_GAMING),             XXXXXXX, TG(_NAV),XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
   XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG,\
