46f5345b45d3e47859c348ebd14e44d6b14e5f0d
Aydiem <3894484+aydiem@users.noreply.github.com>
2020-10-11 23:36:29 -0700

    added numpad layer to romac_plus keymap and made base layer a navigation one.

--- keyboards/kingly_keys/romac_plus/keymaps/aydiem/keymap.c
+++ keyboards/kingly_keys/romac_plus/keymaps/aydiem/keymap.c
@@ -16,23 +16,33 @@
 
 #include QMK_KEYBOARD_H
 
-#define BASE 0
-#define FN 1
+enum layer_number {
+_BASE = 0,
+_NUMP,
+_FN
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	[BASE] = LAYOUT(
+	[_BASE] = LAYOUT(
+		KC_HOME, KC_DEL,       KC_PGUP,
+		KC_END,  KC_UP,        KC_PGDN,
+		KC_LEFT, KC_DOWN,      KC_RIGHT,
+		MO(_FN), MEH(KC_QUOT), MEH(KC_SCLN)
+	),
+
+  [_NUMP] = LAYOUT(
 		KC_7, KC_8, KC_9,
 		KC_4, KC_5, KC_6,
 		KC_1, KC_2, KC_3,
-		MO(1), KC_0, KC_DOT
+		MO(_FN), KC_0, KC_DOT
 	),
   
-	[FN] = LAYOUT(
+	[_FN] = LAYOUT(
 		RGB_TOG, RGB_MOD, RGB_M_P,
 		RGB_HUI, RGB_SAI, RGB_VAI,
 		RGB_HUD, RGB_SAD, RGB_VAD,
-		KC_TRNS, RESET, KC_ENT
+		_______, RESET,   TG(_NUMP)
 	)
 };
 
@@ -45,11 +55,14 @@ void oled_task_user(void) {
   // Host Keyboard Layer Status
   //oled_write_P(PSTR("Let's\nbuild\nsome-\nthing\nto-\nget-\nher!"), false);
   switch (get_highest_layer(layer_state)) {
-    case BASE:
+    case _BASE:
       oled_write_ln_P(PSTR(""), false);
       break;
-    case FN:
+    case _FN:
       oled_write_ln_P(PSTR("FN"), false);
+      break;
+    case _NUMP:
+      oled_write_ln_P(PSTR("Numpad"), false);
       break;
     default:
       // Or use the write_ln shortcut over adding '\n' to the end of your string
