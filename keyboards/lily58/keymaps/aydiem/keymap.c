8876f63f85bebd108b72c07e4d83a310e3000d83
Aydiem <3894484+aydiem@users.noreply.github.com>
2020-10-12 20:15:26 -0700

    Lily58 edit: changed alt from mod tap to just regular alt so no more accidental delete presses

--- keyboards/lily58/keymaps/aydiem/keymap.c
+++ keyboards/lily58/keymaps/aydiem/keymap.c
@@ -32,8 +32,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  MUTE |  Play/Pause|------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/ Enter /     \ BSPC \-----------------------------------------'
- *                   | LGUI |Delete|LCTRL | /LOWER  /       \RAISE \  |Space |Delete| RGUI |
- *                   |      | LAlt |      |/       /         \      \ |      |RCTRL |      |
+ *                   |      |      |      | /LOWER  /       \RAISE \  |Space |Delete|      |
+ *                   | LGUI | LAlt |LCTRL |/       /         \      \ |      |RCTRL | RGUI |
  *                   `----------------------------'           '------''--------------------'
  */
 
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,         KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL, \
   LSFT_T(KC_CAPS),KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,        KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,   KC_MPLY, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-         KC_LGUI, LALT_T(KC_DEL), KC_LCTRL, LT(_LOWER, KC_ENT), LT(_RAISE, KC_BSPC), KC_SPC, RCTL_T(KC_DEL), KC_RGUI \
+                     KC_LGUI, KC_LALT, KC_LCTRL, LT(_LOWER, KC_ENT), LT(_RAISE, KC_BSPC), KC_SPC, RCTL_T(KC_DEL), KC_RGUI \
 ),
 /* GAMING
  * ,-----------------------------------------.                    ,-----------------------------------------.
