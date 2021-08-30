e8d87536d8cb1ea6544e70200a734a940ca92e86
Aydiem <3894484+aydiem@users.noreply.github.com>
2021-08-30 16:56:52 -0700

    removed reset from the default layer.

--- keyboards/tico/keymaps/aydiem/keymap.c
+++ keyboards/tico/keymaps/aydiem/keymap.c
@@ -14,9 +14,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * ,---------------------------------------------------.                    ,------------------------------------------------.
- * | Gaming | tilde |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |  
+ * |        | tilde |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |  
  * |--------+-------+------+------+------+------+------|                    |------+------+------+------+------+------+------|
- * | Numpad | ESC   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   | Home |
+ * |        | ESC   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   | Home |
  * |--------+-------+------+------+------+------+------|                    |------+------+------+------+------+------+------|
  * |        | Tab   |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  =   | End  |
  * |--------+-------+------+------+------+------+------|                    |------+------+------+------+------+------+------|
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------'    '--------------------------------------------------------'
  */
   [_QWERTY] = LAYOUT( \
-  TG(_GAMING),  KC_TILD,    KC_F1,  KC_F2,  KC_F3,   KC_F4,   KC_F5,                                             KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
+  XXXXXXX,  KC_TILD,    KC_F1,  KC_F2,  KC_F3,   KC_F4,   KC_F5,                                             KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
   KC_P2,  KC_ESC,     KC_1,   KC_2,   KC_3,    KC_4,    KC_5,                                              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_HOME, \
   KC_P3,  KC_TAB,     KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  KC_END, \
   KC_P4,  KC_CAPS,    KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_PGUP, \
@@ -299,12 +299,14 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 //Everything here runs whenever a layer state is changed
 layer_state_t layer_state_set_user(layer_state_t state) {
   state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-  /* switch (get_highest_layer(state)) {
+  switch (get_highest_layer(state)) {
     case _GAMING:
-        break;
+      combo_disable()
+      break;
+    case _DOTA:
+      combo_disable()
+      break;
     case _NUMP:
-        break;
-    case _NAV:
         break;
     case _LOWER:
         break;
@@ -313,7 +315,8 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     case _ADJUST:
         break;
     default:
+      combo_enable()
         break;
-    } */
+    }
   return state;
 }
