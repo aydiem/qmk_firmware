36982cb710f2ce00705e6bddc1f2868bf00c9139
Aydiem <3894484+aydiem@users.noreply.github.com>
2021-08-30 20:41:34 -0700

    fixed typo

--- keyboards/tico/keymaps/aydiem/keymap.c
+++ keyboards/tico/keymaps/aydiem/keymap.c
@@ -129,11 +129,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT( \
   XXXXXXX, KC_TILD,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,  KC_F9,  KC_F10,  KC_F11,  KC_F12, \
-  XXXXXXX, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, ______, _______, _______, _______, \
+  XXXXXXX, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______, \
   XXXXXXX, KC_TILD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_MINS, KC_PLUS,  KC_EQL,KC_LCBR, KC_RCBR, XXXXXXX, _______, \
   XXXXXXX,  KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                  KC_CIRC, KC_AMPR, KC_ASTR,KC_LPRN, KC_RPRN, KC_BSLS, _______, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPLY, XXXXXXX, XXXXXXX, KC_UNDS, XXXXXXX,KC_LBRC, KC_RBRC, KC_PIPE, _______,\
-  _______, _______,    _______,_______,KC_LGUI, KC_LALT, KC_LCTRL, LT(_LOWER, KC_ENT), LT(_RAISE, KC_BSPC), KC_SPC,  KC_DEL,  KC_RCTRL,KC_RGUI, KC_LEFT, KC_DOWN, KC_RIGHT \
+  _______, _______, _______, _______,KC_LGUI, KC_LALT, KC_LCTRL, LT(_LOWER, KC_ENT), LT(_RAISE, KC_BSPC), KC_SPC,  KC_DEL,  KC_RCTRL,KC_RGUI, KC_LEFT, KC_DOWN, KC_RIGHT \
 ),
 /* RAISE
  * ,---------------------------------------------------.                    ,------------------------------------------------.
